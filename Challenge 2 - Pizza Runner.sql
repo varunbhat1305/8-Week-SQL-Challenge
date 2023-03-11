274acf1b86b39a629b9b0ec1ed87d194a2549698
Varun Bhat <varunbhat1305@gmail.com>
2023-03-11 21:03:06 +0530

    Update Challenge 2 - Pizza Runner.sql

--- Challenge 2 - Pizza Runner.sql
+++ Challenge 2 - Pizza Runner.sql
@@ -298,16 +298,3 @@ order by runner_id,order_id;
 select runner_id,Round(100*(count(distance)/count(*)),2) as Success_rate
 from run_ord_cl
 group by runner_id;
-
-		#C. Ingredient Optimisation
-#01.What are the standard ingredients for each pizza?
-#02.What was the most commonly added extra?
-#03.What was the most common exclusion?
-#04.Generate an order item for each record in the customers_orders table in the format of one of the following:
-#Meat Lovers
-#Meat Lovers - Exclude Beef
-#Meat Lovers - Extra Bacon
-#Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
-#05.Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
-#for example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
-#06.What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
